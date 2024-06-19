b81ac868f8e03a07f2d9d7028201857a042077ee
NatanaelDominguez <jorgedominguez349@gmail.com>
2024-06-18 23:22:48 -0300

    creando frontend

---
+++ tp5-domingueznatanael-frontend/src/app/components/transaccion-form/transaccion-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-transaccion-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './transaccion-form.component.html',
+  styleUrl: './transaccion-form.component.css'
+})
+export class TransaccionFormComponent {
+
+}
